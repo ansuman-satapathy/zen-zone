9308304c3b9da6ad2909ba49e6694ae959f59149
ansuman-satapathy <satapathyansuman12@gmail.com>
2024-04-20 23:05:14 +0530

    project updated

--- src/main/java/com/app/zenzone/controller/RegistrationController.java
+++ src/main/java/com/app/zenzone/controller/RegistrationController.java
@@ -1,77 +1,8 @@
 package com.app.zenzone.controller;
 
-import com.app.zenzone.model.User;
-import com.app.zenzone.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
 
-@RestController
-@RequestMapping
 public class RegistrationController {
-
-//    @Autowired
-//    private UserService userService;
-//
-//    @GetMapping
-//    public List<User> getAllUsers() {
-//        return userService.getAllUsers();
-//    }
-//
-///*
-//    @PostMapping("/register")
-//    public ResponseEntity<User> registerUser(@RequestBody User user) {
-//        User createdUser = userService.createUser(user);
-//        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
-//    }
-//    @PostMapping("/login")
-//    public ResponseEntity<String> login(@RequestParam String username, @RequestParam String password) {
-//        if (userService.authenticateUser(username, password)) {
-//            return new ResponseEntity<>("Login successful", HttpStatus.OK);
-//        } else {
-//            return new ResponseEntity<>("Invalid username or password", HttpStatus.UNAUTHORIZED);
-//        }
-//    }
-//*/
-//
-//    @PostMapping
-//    public ResponseEntity<User> createUser(@RequestBody User user) {
-//        User createdUser = userService.createUser(user);
-//        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
-//    }
-//
-//    @GetMapping("/{id}")
-//    public ResponseEntity<User> getUserById(@PathVariable Long id) {
-//        Optional<User> user = userService.getUserById(id);
-//        return user.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
-//                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
-//    }
-//
-//    @PutMapping("/{id}")
-//    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
-//        User updatedUser = userService.updateUser(id, user);
-//        if (updatedUser != null) {
-//            return new ResponseEntity<>(updatedUser, HttpStatus.OK);
-//        } else {
-//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//        }
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
-//        userService.deleteUser(id);
-//        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-//    }
-//
-//    @DeleteMapping
-//    public ResponseEntity<Void> deleteAllUsers() {
-//        userService.deleteAllUsers();
-//        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-//    }
 
 }
 
--- src/main/java/com/app/zenzone/model/Habit.java
+++ src/main/java/com/app/zenzone/model/Habit.java
@@ -1,7 +1,5 @@
 package com.app.zenzone.model;
 
-import java.time.LocalDateTime;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
