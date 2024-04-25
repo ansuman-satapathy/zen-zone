3544385edc26b2b936fed6a710ce6bff40f0e5bc
ansuman-satapathy <satapathyansuman12@gmail.com>
2024-04-25 08:44:28 +0530

    minor changes

--- src/main/java/com/app/zenzone/repository/HabitRepository.java
+++ src/main/java/com/app/zenzone/repository/HabitRepository.java
@@ -2,12 +2,10 @@ package com.app.zenzone.repository;
 
 import com.app.zenzone.model.Habit;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HabitRepository extends JpaRepository<Habit, Long> {
 
-    
 }
